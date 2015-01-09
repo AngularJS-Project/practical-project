1e83b5df472d868066b19613db6db7b0f24ccef3
iuli1478 <iuli.iuli17@gmail.com>
2015-01-09 23:33:07 +0200

    a

--- Angular Project/public_html/app/js/controllers/HomeController.js
+++ Angular Project/public_html/app/js/controllers/HomeController.js
@@ -1,6 +1,7 @@
 'use strict';
 
 app.controller('HomeController', function($scope, $rootScope, adsService, pageSize) {
+    $scope.title = "Home";
     adsService.getAds(null,
             function(data) {
                 $scope.ads = data;
