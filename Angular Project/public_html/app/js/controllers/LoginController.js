b0895f14d9dcd293e07bef5b5910d15f5368ff2b
iuli1478 <iuli.iuli17@gmail.com>
2015-01-10 18:16:13 +0200

    styles is home.html

--- Angular Project/public_html/app/js/controllers/LoginController.js
+++ Angular Project/public_html/app/js/controllers/LoginController.js
@@ -1,5 +1,5 @@
 app.controller('LoginController', ['$scope', 'userData', 'userInformation',
-    function($scope, userService, userInformation) {
+    function($scope, userData, userInformation) {
         $scope.login = function(user) {
             userData.login(user);
         };
